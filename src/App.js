fe6af60db9a68efda3c8d4fb169fdfa57c16a24b
Edmond Ma <edjunma@gmail.com>
2020-07-08 00:30:38 -0400

    setup files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/ui/Header';
+import './App.css';
+
+const App = () => {
+	return ( <
+		div className = 'container' >
+		<
+		Header / >
+		<
+		/div>
+	);
+}
+
+export default App;
